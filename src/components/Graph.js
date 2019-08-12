626505e1145f32da440eb7f42f9f9861a5e82f6e
mariana-gamino <mariianagamino@gmail.com>
2019-08-12 16:33:47 -0500

    Ultimo cambio. Horas con select option

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -89,7 +89,7 @@ const Graph = props => {
         })
     } else if(inputValidations(startDate, endDate, startHour, endHour)) {
       loading.innerHTML = '<img src="/loading.gif" width="50px"/>'
-      axios.get(`${baseURL}/get_kpis/1159/${startDate.toString()}/${endDate.toString()}/${startHour.toString().padStart(2,"0")}/${endHour.toString().padStart(2,"0")}`, {
+      axios.get(`${baseURL}/get_kpis/1159/${startDate.toString()}/${endDate.toString()}/${startHour}/${endHour}`, {
       headers: {
         "x-access-token": token,
         "Content-type": "application/json"
@@ -154,11 +154,67 @@ const Graph = props => {
         </div>
         <div className="container" style={{width: "10%"}}>
           <label>Start hour</label><br/>
-          <input type="number" name="startHour" className="hour" placeholder="HH" min="0" max="24" onChange={handleInputs}/>
+          {/* <input type="number" name="startHour" className="hour" placeholder="HH" min="0" max="24" onChange={handleInputs}/> */}
+          <select defaultValue="HH" name="startHour" type="text" className="hour" onChange={handleInputs}>
+            <option value="HH" disabled>HH</option>
+            <option value="00">00</option>
+            <option value="01">01</option>
+            <option value="02">02</option>
+            <option value="03">03</option>
+            <option value="04">04</option>
+            <option value="05">05</option>
+            <option value="06">06</option>
+            <option value="07">07</option>
+            <option value="08">08</option>
+            <option value="09">09</option>
+            <option value="10">10</option>
+            <option value="11">11</option>
+            <option value="12">12</option>
+            <option value="13">13</option>
+            <option value="14">14</option>
+            <option value="15">15</option>
+            <option value="16">16</option>
+            <option value="17">17</option>
+            <option value="18">18</option>
+            <option value="19">19</option>
+            <option value="20">20</option>
+            <option value="21">21</option>
+            <option value="22">22</option>
+            <option value="23">23</option>
+            <option value="24">24</option>
+          </select>
         </div>
         <div className="container" style={{width: "10%"}}>
           <label>End hour</label><br/>
-          <input type="number" name="endHour" className="hour" placeholder="HH" min="0" max="24" onChange={handleInputs}/>
+          {/* <input type="number" name="endHour" className="hour" placeholder="HH" min="0" max="24" onChange={handleInputs}/> */}
+          <select defaultValue="HH" name="endHour" type="text" className="hour" onChange={handleInputs}>
+            <option value="HH" disabled>HH</option>
+            <option value="00">00</option>
+            <option value="01">01</option>
+            <option value="02">02</option>
+            <option value="03">03</option>
+            <option value="04">04</option>
+            <option value="05">05</option>
+            <option value="06">06</option>
+            <option value="07">07</option>
+            <option value="08">08</option>
+            <option value="09">09</option>
+            <option value="10">10</option>
+            <option value="11">11</option>
+            <option value="12">12</option>
+            <option value="13">13</option>
+            <option value="14">14</option>
+            <option value="15">15</option>
+            <option value="16">16</option>
+            <option value="17">17</option>
+            <option value="18">18</option>
+            <option value="19">19</option>
+            <option value="20">20</option>
+            <option value="21">21</option>
+            <option value="22">22</option>
+            <option value="23">23</option>
+            <option value="24">24</option>
+          </select>
         </div>
         <div className="container">
           <label>Refresh</label><br/>
